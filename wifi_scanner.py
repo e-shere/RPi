1baf816b122be23adfe28d0ad75680cf3ed4ded4
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-10-23 08:32:49 +0000

    created wifi scanner

---
+++ wifi_scanner.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+import os
+import time
+
+
+def start_tshark():
+    os.system("sudo tshark -i wlan0mon -T fields -e frame.time -e wlan.sa -e radiotap.channel.freq -e radiotap.dbm_antsignal > ~/Documents/$(date '+%Y_%m_%d_%H_%M_%S').txt")
+
+def channel_hop():
+    channels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    wait = 1
+    i = 0
+    while True:
+        os.system('iw dev wlan0mon set channel %d'%channels[i])
+        i = (i+1)%len(channels)
+        time.sleep(wait)
+
+if __name__=='__main__':
+    os.system('sudo ifconfig wlan0mon down')
+    os.system('sudo iwconfig wlan0mon mode monitor')
+    os.system('sudo ifconfig wlan0mon up')
+    p1 = Process(target=start_tshark)
+    p1.start()
+    p2 = Process(target=channel_hop)
+    p2.start()
