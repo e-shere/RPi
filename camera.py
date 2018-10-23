bd1a522a4678945e33041fa1812b85f21e3fbcd3
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-10-23 13:56:35 +0000

    added shooting video when motion is detected

---
+++ camera.py
@@ -0,0 +1,23 @@
+import picamera
+import picamera.array
+import numpy as np
+
+class MotionDetector(picamera.array.PiMotionAnalysis):
+    def analyse(self,a):
+        a = np.sqrt(np.square(a['x'].astype(np.float)) + np.square(a['y'].astype(np.float))).clip(0,255).astype(np.uint8)
+
+        if (a>30).sum() >3:
+            print("Motion detected")
+
+with picamera.PiCamera() as camera:
+    counter=1
+    while True:
+        location = '/root/Videos/'+str(counter)+'.h264'
+        camera.resolution = (1640,1232)
+        camera.rotation = 180
+        camera.framerate = 30
+        camera.start_recording(location,format="h264",motion_output=MotionDetector(camera))
+        camera.wait_recording(30)
+        camera.stop_recording()
+        counter+=1
+        print(counter)
