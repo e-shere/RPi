c185c9b85763c414998eccbab14587d7a12762bd
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-11-22 21:15:09 +0000

    removed file conversion from RPi

--- renaming.py
+++ renaming.py
@@ -1,6 +1,6 @@
 import os
 for filename in os.listdir('/root/RPi/h264/'):
     date = os.popen('date -r "/root/RPi/h264/'+filename+'" "+%Y_%m_%d_%H_%M_%S"').read().strip("\n")
-    os.system("ffmpeg -r 30 -y -i /root/RPi/h264/"+filename+" -c copy /root/RPi/mp4/"+date+".mp4 > /dev/null 2>&1")
+    os.system("cp /root/RPi/h264/"+filename+" /root/RPi/dated/"+date+".h264")
 
 
