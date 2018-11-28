4fcae5604b2ec97a5b7c3c3c6460f256a4d0b37e
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-11-28 19:46:12 +0000

    conversion program on server/laptop

---
+++ converting.py
@@ -0,0 +1,6 @@
+import os
+for filename in os.listdir('/root/RPi/dated/'):
+    new_name = filename.split(".")[0]
+    os.system("ffmpeg -r 30 -y -i ~/dated/"+filename+" -c copy ~/mp4/"+new_name+".mp4 > /dev/null 2>&1")
+
+
