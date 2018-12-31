eec0423e5539d9b021722c46b09e8b1b1b74c57d
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-12-31 20:45:30 +0000

    saves files of spoken audio

--- piaudio_new.py
+++ piaudio_new.py
@@ -17,8 +17,10 @@ import contextlib
 import wave
 import webrtcvad
 import datetime
+from pathlib import Path
 
 def write_wave(path, audio, sample_rate):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
     with contextlib.closing(wave.open(path, 'wb')) as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
@@ -34,13 +36,15 @@ def main():
     CHUNK = int(RATE*FRAME_DURATION/1000)
     PADDING_DURATION = 300
     num_padding_frames = int(PADDING_DURATION/FRAME_DURATION)
-    FILE_PATTERN = '~/audio/%s.wav'
+    FILE_PATTERN = 'audio/%s.wav'
 
     p = pyaudio.PyAudio()
     ring_buffer = collections.deque(maxlen=num_padding_frames)
     vad = webrtcvad.Vad(3)
     speech = False
     num_speech = 0
+    voiced_frames = []
+    time = ""
 
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
@@ -55,19 +59,25 @@ def main():
 
         if not speech:
             ring_buffer.append((data, is_speech))
-            num_speech = len([f for f, speech in ring_buffer if speech])
+            num_speech = len([data for data, speech in ring_buffer if speech])
             if num_speech>0.9*num_padding_frames:
                 speech = True
                 time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
                 print("\n" + time + "\n")
                 print("you started speaking")
+                for data, speech in ring_buffer:
+                    voiced_frames.append(data)
+                ring_buffer.clear()
         else:
             ring_buffer.append((data, is_speech))
-            num_notspeech = len([f for f, speech in ring_buffer if not speech])
+            voiced_frames.append(data)
+            num_notspeech = len([data for data, speech in ring_buffer if not speech])
             if num_notspeech>0.9*num_padding_frames:
                 speech=False
-                time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
-                print("\n" + time + "\n")
+                sentence = b''.join([f for f in voiced_frames])
+                write_wave(FILE_PATTERN%time,sentence,RATE)
+                ring_buffer.clear()
+                voiced_frames=[]
                 print("you finished speaking")
             else:
                 print("you are speaking")
