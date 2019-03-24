8b59c7fbd6057223d651340b0a65d8b3942c8928
Liza <elizaveta.sheremetyeva@gmail.com>
2019-03-24 20:50:56 +0000

    Delete converting.py

--- converting.py
+++
@@ -1,6 +0,0 @@
-import os
-for filename in os.listdir('~/dated/'):
-    new_name = filename.split(".")[0]
-    os.system("ffmpeg -r 30 -y -i ~/dated/"+filename+" -c copy ~/mp4/"+new_name+".mp4 > /dev/null 2>&1")
-
-
